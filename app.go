4d9c36369d88fab9fde74880a9abed07d2d22434
phongvd8 <phongvd8@fpt.com.vn>
2022-08-13 03:00:47 +0700

    first commit

---
+++ app.go
@@ -0,0 +1,232 @@
+// app.go
+
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
+)
+
+type App struct {
+	Router *mux.Router
+	DB     *sql.DB
+}
+
+func (a *App) Initialize(user, password, dbname string) {
+	connectionString := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", user, password, dbname)
+	var err error
+	a.DB, err = sql.Open("postgres", connectionString)
+	if err != nil {
+		log.Fatal(err)
+	}
+	a.Router = mux.NewRouter()
+	a.initializeRoutes()
+}
+
+func (a *App) Run(addr string) {
+	log.Fatal(http.ListenAndServe(addr, a.Router))
+}
+
+func (a *App) initializeRoutes() {
+	a.Router.HandleFunc("/issue", a.createIssue).Methods("POST")
+	a.Router.HandleFunc("/error", a.createError).Methods("POST")
+	a.Router.HandleFunc("/issue/{id:[a-zA-Z0-9]*}", a.getStatusIssue).Methods("GET")
+	a.Router.HandleFunc("/job/{id:[a-zA-Z0-9]*}", a.getJob).Methods("GET")
+	a.Router.HandleFunc("/issue/{id:[a-zA-Z0-9]*}", a.deleteIssue).Methods("DELETE")
+	a.Router.HandleFunc("/issue/{id:[a-zA-Z0-9]*}", a.updateIssue).Methods("UPDATE")
+	a.Router.HandleFunc("/issue", a.getIssue).Methods("GET")
+	a.Router.HandleFunc("/issue/{id:[a-zA-Z0-9]*}", a.GetIssueByJiraID).Methods("GET")
+}
+
+func respondWithError(w http.ResponseWriter, code int, message string) {
+	respondWithJSON(w, code, map[string]string{"error": message})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
+
+func (a *App) createIssue(w http.ResponseWriter, r *http.Request) {
+	var i Issues
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&i); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+	defer r.Body.Close()
+
+	if i.ErrorCode == "" {
+		if err := PushIssueToBacklogJira(); err != nil {
+			respondWithError(w, http.StatusInternalServerError, "Unable to push issue to backlog of Jira")
+		}
+	} else {
+		if err := i.createIssue(a.DB); err != nil {
+			respondWithError(w, http.StatusInternalServerError, err.Error())
+			return
+		}
+		respondWithJSON(w, http.StatusCreated, i)
+	}
+}
+
+func PushIssueToBacklogJira() error {
+	return nil
+}
+
+func PushIsseToProject(projectID, issueType, assignee, reporter, environment, content string) error {
+	url := "http://10.0.0.4:8080/rest/api/2/issue"
+	method := "POST"
+	data := fmt.Sprintf(`{
+        "update": {      
+        },
+        "fields": {
+        "project": {
+            "id": %s
+        },
+        "summary": summary,
+        "issuetype": {
+            "id": %s
+        },
+        "assignee": {
+            "name": %s
+        },
+        "reporter": {
+            "name": %s
+        },
+        
+        "environment": %s,
+        "description": %s,
+        
+        "components": [
+            {
+            "id": "10000"
+            }
+        ]
+        }
+    }`, projectID, issueType, assignee, reporter, environment, content)
+	payload := strings.NewReader(data)
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, payload)
+
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	req.Header.Add("Content-type", "application/json")
+
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	fmt.Println(string(body))
+	return nil
+}
+
+func (a *App) createError(w http.ResponseWriter, r *http.Request) {
+	var e ErrorStore
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&e); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+	defer r.Body.Close()
+	if err := e.createError(a.DB); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondWithJSON(w, http.StatusCreated, e)
+}
+
+func (a *App) getStatusIssue(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	issueJiraID := vars["issue_jira_id"]
+	issue := Issues{
+		IssueJiraID: issueJiraID,
+	}
+	status, err := issue.getStatusIssue(a.DB, issueJiraID)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondWithJSON(w, http.StatusOK, status)
+}
+
+func (a *App) getJob(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	issueJiraID := vars["issue_jira_id"]
+	issue := Issues{
+		IssueJiraID: issueJiraID,
+	}
+	issue.ApiJob(a.DB, issueJiraID)
+
+	respondWithJSON(w, http.StatusOK, "")
+}
+
+func (a *App) deleteIssue(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	issueJiraID := vars["issue_jira_id"]
+	issue := Issues{
+		IssueJiraID: issueJiraID,
+	}
+	if err := issue.DeleteIssue(a.DB, issueJiraID); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondWithJSON(w, http.StatusOK, map[string]string{"delete": "success"})
+}
+
+func (a *App) updateIssue(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	issueJiraID := vars["issue_jira_id"]
+	status := vars["status"]
+	issue := Issues{
+		IssueJiraID: issueJiraID,
+	}
+	if err := issue.UpdateIssueStatusInDB(a.DB, status); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondWithJSON(w, http.StatusOK, issue)
+}
+
+func (a *App) getIssue(w http.ResponseWriter, r *http.Request) {
+	issue, err := a.GetIssue(a.DB)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondWithJSON(w, http.StatusOK, issue)
+}
+
+func (a *App) GetIssueByJiraID(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	issueJiraID := vars["issue_jira_id"]
+	issue := Issues{
+		IssueJiraID: issueJiraID,
+	}
+	if err := issue.GetIssueByJiraID(a.DB, issueJiraID); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondWithJSON(w, http.StatusOK, issue)
+}
---
+++ model.go
@@ -0,0 +1,191 @@
+// model.go
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+type BaseModel struct {
+	ID        int `json:"id"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+type Issues struct {
+	BaseModel
+	TenantID    string `json:"tenantId"`
+	VpcID       string `json:"vpcId"`
+	RegionID    string `json:"regionId"`
+	IssueJiraID string `json:"issueJiraID"`
+	Name        string `json:"name"`
+	DataLog     string `json:"dataLog"`
+	ErrorCode   string `json:"errorCode"`
+	Status      string `json:"status"`
+	Service     string `json:"service"`
+}
+
+type StepLog struct {
+	BaseModel
+	ReporterID    string `json:"reporterId"`
+	IssueID       string `json:"issueID"`
+	Description   string `json:"description"`
+	SupporterJira string `json:"supporterJira"`
+	Status        string `json:"status"`
+}
+
+type Reporter struct {
+	BaseModel
+	Username string `json:"username"`
+	Email    string `json:"email"`
+}
+
+type ErrorStore struct {
+	BaseModel
+	ErrorCode   string `json:"errorCode"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Service     string `json:"service"`
+}
+
+type IssueResponse struct {
+	Expand string `json:"expand"`
+	ID     string `json:"id"`
+	Self   string `json:"self"`
+	Key    string `json:"key"`
+	Fields struct {
+		Status struct {
+			Self           string `json:"self"`
+			Description    string `json:"description"`
+			IconURL        string `json:"iconUrl"`
+			Name           string `json:"name"`
+			ID             string `json:"id"`
+			StatusCategory struct {
+				Self      string `json:"self"`
+				ID        int    `json:"id"`
+				Key       string `json:"key"`
+				ColorName string `json:"colorName"`
+				Name      string `json:"name"`
+			} `json:"statusCategory"`
+		} `json:"status"`
+	} `json:"fields"`
+}
+
+func (issue *Issues) ApiJob(db *sql.DB, issueJiraID string) {
+	for {
+		status1 := issue.GetIssueStatusFromDB(db, issueJiraID)
+		status2 := issue.GetJiraIssueStatus(db, issueJiraID)
+		if status1 != status2 {
+			if err := issue.UpdateIssueStatusInDB(db, status2); err != nil {
+				fmt.Printf("Unable to update status of issue in DB")
+			}
+		}
+		time.Sleep(30 * time.Second)
+	}
+}
+
+func (issue *Issues) GetIssueStatusFromDB(db *sql.DB, issueJiraID string) string {
+	var status string
+	// db.QueryRow("SELECT status FROM issues WHERE issue_jira_id = ?", issueJiraID)
+	err := db.QueryRow("SELECT status FROM issues WHERE issue_jira_id=$1", issueJiraID).Scan(status)
+	if err != nil {
+		fmt.Printf("Unable to query status with issue_jira_id from issues table")
+	}
+	return status
+}
+
+func (issue *Issues) GetJiraIssueStatus(db *sql.DB, issueJiraID string) string {
+	url := fmt.Sprintf("http://10.0.0.4:8080/rest/api/2/issue/%s?fields=status", issueJiraID)
+	method := "GET"
+	var issueResponse IssueResponse
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, nil)
+
+	if err != nil {
+		fmt.Printf("Unable to create new request to get issue of Jira: [%s]", err.Error())
+		return ""
+	}
+	req.Header.Add("Content-Type", "application/json")
+
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Printf("Unable to perform request getting Issue Jira: [%s]", err.Error())
+		return ""
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Printf("Unable to read body of response from Jira: [%s]\n", err.Error())
+		return ""
+	}
+	dataBody := []byte(body)
+	err = json.Unmarshal(dataBody, &issueResponse)
+	if err != nil {
+		fmt.Printf("Unable to Unmarshal response body from jira: [%s]\n", err.Error())
+	}
+
+	return issueResponse.Fields.Status.Name
+}
+
+func (errorStore *ErrorStore) createError(db *sql.DB) error {
+	statement := fmt.Sprintf("INSERT INTO error_store(error_code, name, discription, service) VALUES('%s', '%s', '%s', '%s') RETURNING id",
+		errorStore.ErrorCode, errorStore.Name, errorStore.Description, errorStore.Service)
+	_, err := db.Exec(statement)
+	if err != nil {
+		return err
+	}
+	err = db.QueryRow("SELECT LAST_INSERT_ID()").Scan(&errorStore.ID)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (issue *Issues) createIssue(db *sql.DB) error {
+	statement := fmt.Sprintf("INSERT INTO issues(tenant_id, vpc_id, region_id, issue_jira_id, name, data_log, error_code, status, service) VALUES('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s') RETURNING id",
+		issue.TenantID, issue.VpcID, issue.RegionID, issue.IssueJiraID, issue.Name, issue.DataLog, issue.ErrorCode, issue.Status, issue.Service)
+	_, err := db.Exec(statement)
+	if err != nil {
+		return err
+	}
+	err = db.QueryRow("SELECT LAST_INSERT_ID()").Scan(&issue.ID)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (issue *Issues) getStatusIssue(db *sql.DB, issueJiraID string) (string, error) {
+	var status string
+	err := db.QueryRow("SELECT status FROM issues WHERE issue_jira_id=$1", issueJiraID).Scan(status)
+	if err != nil {
+		fmt.Printf("Unable to query status with issue_jira_id from issues table")
+		return "", fmt.Errorf("Unable to query status with issue_jira_id from issues table: [%s]", err.Error())
+	}
+	return status, nil
+}
+
+func (issue *Issues) UpdateIssueStatusInDB(db *sql.DB, status string) error {
+	_, err := db.Exec("UPDATE issues SET status=$1", status)
+	return err
+}
+
+func (issue *Issues) DeleteIssue(db *sql.DB, issueJiraID string) error {
+	_, err := db.Exec("DELETE FROM issues WHERE issue_jira_id=$1", issueJiraID)
+	return err
+}
+
+func (app *App) GetIssue(db *sql.DB) (sql.Result, error) {
+	issue, err := db.Exec("SELECT * FROM issues")
+	return issue, err
+}
+
+func (issue *Issues) GetIssueByJiraID(db *sql.DB, issueJiraID string) error {
+	var i Issues
+	return db.QueryRow("SELECT * FROM issues WHERE issue_jira_id=$1=$1", issueJiraID).Scan(i)
+}
